e96fa4ebd1d267f292042733df521952596be5e8
BrnMarq <brnmarq@gmail.com>
2026-01-15 19:27:03 -0400

    ✨Added conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" }, -- Lazy load plugin
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          python = { "ruff_format" }, -- Tries ruff_format first, then black as a fallback
+          lua = { "stylua" },
+        },
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*",
+        callback = function(args)
+          require("conform").format({ bufnr = args.buf })
+        end,
+      })
+    end,
+  },
+  {
+    -- Companion plugin to automatically install formatters via mason.nvim
+    "zapling/mason-conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      -- This plugin will automatically scan your conform.nvim settings
+      -- and ensure the formatters (like "stylua", "black", "prettier") are installed.
+    },
+    config = function()
+      require("mason-conform").setup()
+    end,
+  },
+}
