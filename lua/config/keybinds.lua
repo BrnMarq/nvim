bf02d6e8c423c7edb3b4384412c7cd0710830553
BrnMarq <brnmarq@gmail.com>
2026-01-15 19:07:34 -0400

    ✨ Configure treesitter-textobjects

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -4,10 +4,12 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>p", '"+p')
-vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>o", 'mzo<esc>`z')
+vim.keymap.set("n", "<leader>O", 'mzO<esc>`z')
 
 -- Insert mode keybinds
 vim.keymap.set("i", "jj", "<Esc>")
 
 -- Visual mode keybinds
 vim.keymap.set('v', '"', 'c""<esc>P')
+vim.keymap.set("v", "<leader>y", '"+y')
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,49 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require 'nvim-treesitter'.setup {
-      highlight = { enable = true },
-      indent = { enable = true },
-      autotage = { enable = true },
-      incremental_section = { enable = true },
-      semantic_tokens = { enable = true }
-    }
-    require 'nvim-treesitter'.install { 'lua', 'javascript', 'tsx', 'typescript', 'python', 'cpp' }
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require 'nvim-treesitter'.setup {
+        highlight = { enable = true },
+        indent = { enable = true },
+        autotage = { enable = true },
+      }
+      require 'nvim-treesitter'.install { 'lua', 'javascript', 'tsx', 'typescript', 'python', 'cpp' }
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    init = function()
+      vim.g.no_plugin_maps = true
+    end,
+    config = function()
+      require("nvim-treesitter-textobjects").setup {
+        select = {
+          lookahead = true,
+          selection_modes = {
+            ['@parameter.outer'] = 'v',
+            ['@function.outer'] = 'V',
+          },
+          include_surrounding_whitespace = false,
+        },
+      }
+
+      vim.keymap.set({ "x", "o" }, "am", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "im", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@function.inner", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ac", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@class.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ic", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@class.inner", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "as", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@local.scope", "locals")
+      end)
+    end
+  }
 }
