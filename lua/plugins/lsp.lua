9c7e5b4f30ac673287cee7373027b3ee9a3643fe
BrnMarq <brnmarq@gmail.com>
2026-01-28 20:56:51 -0400

    ✨ Added autoinstallation

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,7 +2,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      'saghen/blink.cmp',
+      "saghen/blink.cmp",
       {
         "folke/lazydev.nvim",
         opts = {
@@ -13,35 +13,19 @@ return {
       },
     },
     config = function()
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
       vim.lsp.config("lua_ls", {
         capabilites = capabilites,
         diagnostics = {
-          globals = { 'vim' },
+          globals = { "vim" },
         },
       })
-      vim.lsp.enable({ "lua_ls" })
-
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          local c = vim.lsp.get_client_by_id(args.data.client_id)
-          if not c then return end
-
-          if vim.bo.filetype == "lua" then
-            -- Format the current buffer on save
-            vim.api.nvim_create_autocmd('BufWritePre', {
-              buffer = args.buf,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
-              end,
-            })
-          end
-        end,
-      })
       vim.lsp.config("pyright", {
-        capabilities = capabilities
+        capabilities = capabilities,
       })
+
+      vim.lsp.enable({ "lua_ls" })
       vim.lsp.enable({ "pyright" })
     end,
-  }
+  },
 }
