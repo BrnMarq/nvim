105c5f5ebbeabf6d499956c7f041acb31c6304c3
BrnMarq <brnmarq@gmail.com>
2026-02-10 16:46:09 -0400

    ✨ Added quickfix control with keybinds

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -4,12 +4,14 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>p", '"+p')
-vim.keymap.set("n", "<leader>o", 'mzo<esc>`z')
-vim.keymap.set("n", "<leader>O", 'mzO<esc>`z')
+vim.keymap.set("n", "<leader>o", "mzo<esc>`z")
+vim.keymap.set("n", "<leader>O", "mzO<esc>`z")
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>")
 
 -- Insert mode keybinds
 vim.keymap.set("i", "jj", "<Esc>")
 
 -- Visual mode keybinds
-vim.keymap.set('v', '"', 'c""<esc>P')
+vim.keymap.set("v", '"', 'c""<esc>P')
 vim.keymap.set("v", "<leader>y", '"+y')
